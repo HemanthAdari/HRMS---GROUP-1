0af2bbb57bb289ff35d69493eaf7f7aa5366db76
Krishnaveni K S <krishna.veni.28@outlook.com>
2025-09-17 11:19:34 +0530

    Delete Attendence.sql

--- Attendence.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE attendance (
-    attendance_id INT AUTO_INCREMENT PRIMARY KEY,
-    user_id INT NOT NULL,
-    check_in DATETIME,
-    check_out DATETIME,
-    status ENUM('Present', 'Absent', 'Leave', 'Holiday') DEFAULT 'Present',
-    remarks VARCHAR(255),
-    FOREIGN KEY (user_id) REFERENCES users(user_id) ON DELETE CASCADE
-);
